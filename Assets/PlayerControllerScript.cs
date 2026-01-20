1d6fe9d463efb56cd62b2c44cad76f968d297ef5
jlcitadel-glitch <jlcitadel@gmail.com>
2026-01-20 00:42:59 -0500

    DirectionalMovement
    Set up directional movement framework. Laid out basics for Jumping. Want to add variable jump based on press/release.
    
    Need to add a directional change on the X/Y axis when changing directions.
    
    Addressed gravity/friction.

---
+++ Assets/PlayerControllerScript.cs
@@ -0,0 +1,46 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+    [Header("Player Component References")]
+    [SerializeField] Rigidbody2D rb;
+
+    [Header("Player Settings")]
+    [SerializeField] float speed;
+    [SerializeField] float jumpingPower;
+
+    [Header("Grounding")]
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] Transform groundCheck;
+
+    private float horizontal;
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+    }
+
+    #region PLAYER_CONTROLS
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(context.performed && IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1f, 0.1f), CapsuleDirection2D.Horizontal, 0, groundLayer);
+    }
+
+    #endregion
+
+}
